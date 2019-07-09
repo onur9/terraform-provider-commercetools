328189e3f04cafe1c3fbf85b1ecbebb7b747521b
David Weterings <david@davidweterings.nl>
2019-07-09 17:46:29 +0200

    Implement new resource product discount

--- commercetools/provider.go
+++ commercetools/provider.go
@@ -3,129 +3,119 @@ package commercetools
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/labd/commercetools-go-sdk/ctutils"
-	"github.com/labd/commercetools-go-sdk/platform"
+	"github.com/hashicorp/terraform/helper/mutexkv"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/labd/commercetools-go-sdk/commercetools"
 	"golang.org/x/oauth2/clientcredentials"
 )
 
-func init() {
-	// Set descriptions to support markdown syntax, this will be used in document generation
-	// and the language server.
-	schema.DescriptionKind = schema.StringMarkdown
-}
-
 // Provider returns a terraform.ResourceProvider.
-func New(version string) func() *schema.Provider {
-	return func() *schema.Provider {
-		p := &schema.Provider{
-			Schema: map[string]*schema.Schema{
-				"client_id": {
-					Type:        schema.TypeString,
-					Required:    true,
-					DefaultFunc: schema.EnvDefaultFunc("CTP_CLIENT_ID", nil),
-					Description: "The OAuth Client ID for a commercetools platform project. https://docs.commercetools.com/http-api-authorization",
-					Sensitive:   true,
-				},
-				"client_secret": {
-					Type:        schema.TypeString,
-					Required:    true,
-					DefaultFunc: schema.EnvDefaultFunc("CTP_CLIENT_SECRET", nil),
-					Description: "The OAuth Client Secret for a commercetools platform project. https://docs.commercetools.com/http-api-authorization",
-					Sensitive:   true,
-				},
-				"project_key": {
-					Type:        schema.TypeString,
-					Required:    true,
-					DefaultFunc: schema.EnvDefaultFunc("CTP_PROJECT_KEY", nil),
-					Description: "The project key of commercetools platform project. https://docs.commercetools.com/getting-started",
-					Sensitive:   true,
-				},
-				"scopes": {
-					Type:        schema.TypeString,
-					Required:    true,
-					DefaultFunc: schema.EnvDefaultFunc("CTP_SCOPES", nil),
-					Description: "A list as string of OAuth scopes assigned to a project key, to access resources in a commercetools platform project. https://docs.commercetools.com/http-api-authorization",
-				},
-				"api_url": {
-					Type:        schema.TypeString,
-					Required:    true,
-					DefaultFunc: schema.EnvDefaultFunc("CTP_API_URL", nil),
-					Description: "The API URL of the commercetools platform. https://docs.commercetools.com/http-api",
-				},
-				"token_url": {
-					Type:        schema.TypeString,
-					Required:    true,
-					DefaultFunc: schema.EnvDefaultFunc("CTP_AUTH_URL", nil),
-					Description: "The authentication URL of the commercetools platform. https://docs.commercetools.com/http-api-authorization",
-				},
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"client_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"CTP_CLIENT_ID",
+				}, nil),
+				Description: "CommercesTools Client ID",
 			},
-			ResourcesMap: map[string]*schema.Resource{
-				"commercetools_api_client":         resourceAPIClient(),
-				"commercetools_api_extension":      resourceAPIExtension(),
-				"commercetools_cart_discount":      resourceCartDiscount(),
-				"commercetools_channel":            resourceChannel(),
-				"commercetools_custom_object":      resourceCustomObject(),
-				"commercetools_customer_group":     resourceCustomerGroup(),
-				"commercetools_discount_code":      resourceDiscountCode(),
-				"commercetools_product_type":       resourceProductType(),
-				"commercetools_project_settings":   resourceProjectSettings(),
-				"commercetools_shipping_method":    resourceShippingMethod(),
-				"commercetools_shipping_zone_rate": resourceShippingZoneRate(),
-				"commercetools_shipping_zone":      resourceShippingZone(),
-				"commercetools_state":              resourceState(),
-				"commercetools_store":              resourceStore(),
-				"commercetools_subscription":       resourceSubscription(),
-				"commercetools_tax_category_rate":  resourceTaxCategoryRate(),
-				"commercetools_tax_category":       resourceTaxCategory(),
-				"commercetools_category":           resourceCategory(),
-				"commercetools_type":               resourceType(),
+			"client_secret": {
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"CTP_CLIENT_SECRET",
+				}, nil),
+				Description: "CommercesTools Client Secret",
 			},
-		}
-		p.ConfigureContextFunc = providerConfigure(version, p)
-		return p
+			"project_key": {
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"CTP_PROJECT_KEY",
+				}, nil),
+				Description: "CommercesTools Project key",
+			},
+			"token_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"CTP_AUTH_URL",
+				}, "https://auth.sphere.io"),
+				Description: "CommercesTools Token URL",
+			},
+			"api_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"CTP_API_URL",
+				}, "https://api.sphere.io"),
+				Description: "CommercesTools API URL",
+			},
+			"scopes": {
+				Type:     schema.TypeString,
+				Optional: true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"CTP_SCOPES",
+				}, nil),
+				Description: "CommercesTools Scopes",
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"commercetools_api_client":         resourceAPIClient(),
+			"commercetools_api_extension":      resourceAPIExtension(),
+			"commercetools_subscription":       resourceSubscription(),
+			"commercetools_project_settings":   resourceProjectSettings(),
+			"commercetools_type":               resourceType(),
+			"commercetools_channel":            resourceChannel(),
+			"commercetools_product_discount":   resourceProductDiscount(),
+			"commercetools_product_type":       resourceProductType(),
+			"commercetools_shipping_method":    resourceShippingMethod(),
+			"commercetools_shipping_zone_rate": resourceShippingZoneRate(),
+			"commercetools_store":              resourceStore(),
+			"commercetools_tax_category":       resourceTaxCategory(),
+			"commercetools_tax_category_rate":  resourceTaxCategoryRate(),
+			"commercetools_shipping_zone":      resourceShippingZone(),
+			"commercetools_state":              resourceState(),
+		},
+		ConfigureFunc: providerConfigure,
 	}
 }
 
-func providerConfigure(version string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		clientID := d.Get("client_id").(string)
-		clientSecret := d.Get("client_secret").(string)
-		projectKey := d.Get("project_key").(string)
-		scopesRaw := d.Get("scopes").(string)
-		apiURL := d.Get("api_url").(string)
-		authURL := d.Get("token_url").(string)
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	projectKey := d.Get("project_key").(string)
 
-		oauthScopes := strings.Split(scopesRaw, " ")
-
-		oauth2Config := &clientcredentials.Config{
-			ClientID:     clientID,
-			ClientSecret: clientSecret,
-			Scopes:       oauthScopes,
-			TokenURL:     fmt.Sprintf("%s/oauth/token", authURL),
-		}
-
-		httpClient := &http.Client{
-			Transport: ctutils.DebugTransport,
-		}
-
-		client, err := platform.NewClient(&platform.ClientConfig{
-			URL:         apiURL,
-			Credentials: oauth2Config,
-			UserAgent:   fmt.Sprintf("terraform-provider-commercetools/%s", version),
-			HTTPClient:  httpClient,
-		})
-
-		if err != nil {
-			return nil, diag.FromErr(err)
-		}
-		return client.WithProjectKey(projectKey), nil
+	scopesRaw := d.Get("scopes").(string)
+	var scopes []string
+	if scopesRaw == "" {
+		scopes = []string{fmt.Sprintf("manage_project:%s", projectKey)}
+	} else {
+		scopes = strings.Split(scopesRaw, " ")
 	}
+
+	oauth2Config := &clientcredentials.Config{
+		ClientID:     d.Get("client_id").(string),
+		ClientSecret: d.Get("client_secret").(string),
+		Scopes:       scopes,
+		TokenURL:     fmt.Sprintf("%s/oauth/token", d.Get("token_url").(string)),
+	}
+	httpClient := oauth2Config.Client(context.TODO())
+
+	client := commercetools.New(&commercetools.Config{
+		ProjectKey:   projectKey,
+		URL:          d.Get("api_url").(string),
+		HTTPClient:   httpClient,
+		LibraryName:  "terraform-provider-commercetools",
+		ContactURL:   "https://labdigital.nl",
+		ContactEmail: "opensource@labdigital.nl",
+	})
+
+	return client, nil
 }
 
 // This is a global MutexKV for use within this plugin.
-var ctMutexKV = NewMutexKV()
+var ctMutexKV = mutexkv.NewMutexKV()
