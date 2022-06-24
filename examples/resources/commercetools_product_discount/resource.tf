445b8245efa33b7ca11245acf8363b6eff0ee122
Onur Gunduz <o.gunduz@labdigital.nl>
2022-06-24 18:34:54 +0200

    Add product discount examples

--- examples/resources/commercetools_product_discount/resource.tf
+++ examples/resources/commercetools_product_discount/resource.tf
@@ -1,25 +1,42 @@
-resource "commercetools_product_discount" "my_product_discount" {
-  name        = {
-    en = "My product discount name"
+resource "commercetools_product_discount" "my-product-discount" {
+  key = "my-product-discount-key"
+  name = {
+    en = "Product discount name"
   }
-  key         = "my-product-discount-key"
   description = {
-    en = "My product discount description"
-  }
-  predicate   = "1=1"
-  sort_order  = "0.2"
-  is_active   = false
-  valid_from  = "2021-01-01T00:00:00.000Z"
-  valid_until = "2022-01-01T00:00:00.000Z"
+    en = "Product discount description"
+    }
+  predicate              = "1=1"
+  sort_order             = "0.9"
+  is_active              = true
+  valid_from             = "2018-01-02T15:04:05Z"
+  valid_until            = "2019-01-02T15:04:05Z"
+
   value {
-    type = "absolute"
+    type      = "relative"
+    permyriad = 1000
+  }
+}
+
+resource "commercetools_product_discount" "my-product-discount-absolute" {
+  key = "my-product-discount-absolute-key"
+  name = {
+    en = "Product discount name"
+  }
+  description = {
+    en = "Product discount description"
+    }
+  predicate              = "1=1"
+  sort_order             = "0.9"
+  is_active              = true
+  valid_from             = "2018-01-02T15:04:05Z"
+  valid_until            = "2019-01-02T15:04:05Z"
+
+  value {
+    type      = "absolute"
     money {
       currency_code = "EUR"
-      cent_amount   = 50
-    }
-    money {
-      currency_code = "CHF"
-      cent_amount   = 1
+      cent_amount   = 500
     }
   }
 }
